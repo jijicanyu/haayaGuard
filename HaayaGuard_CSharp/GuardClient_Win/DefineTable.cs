60bc80961a64b378fb628444ab4ad2b138c92c65
xieqj <xqj198404@gmail.com>
2014-12-22 10:40:08 +0800

    201412221040

---
+++ HaayaGuard_CSharp/GuardClient_Win/DefineTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Haaya.GuardClient
+{
+   internal class DefineTable
+    {
+       public static readonly string ServerHost = ConfigurationManager.AppSettings["ServerHost"];
+       public static readonly int ServerPort =int.Parse(ConfigurationManager.AppSettings["ServerPort"]);
+    }
+}
