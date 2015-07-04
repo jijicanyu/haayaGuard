3012a08ffd4bac80d12bffb347f686abc039b12a
hsieh jing <xqj198404@gmail.com>
2015-07-04 12:15:35 +0800

    Test

---
+++ TestDemo/Java/src/haayaGuard/Java/AppMain.java
@@ -0,0 +1,13 @@
+package haayaGuard.Java;
+
+public class AppMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestDemo/Java/src/haayaGuard/Java/NetFactory.java
@@ -0,0 +1,13 @@
+package haayaGuard.Java;
+
+public class NetFactory {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
