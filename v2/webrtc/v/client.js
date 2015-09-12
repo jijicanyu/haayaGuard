4ff01a80751243577f1f6902336330cd10041d27
hsieh jing <xqj198404@gmail.com>
2015-09-12 16:48:21 +0800

    添加ice和用户验证

--- v2/webrtc/v/client.js
+++ v2/webrtc/v/client.js
@@ -1,15 +1,54 @@
 ﻿/// <reference path="lib/jquery-1.8.2.min.js" />
-var userVaildateProvider = {
+var rtc = null;
+var videos = document.getElementById("videos");
+var pageObject = {
     session: "",
     loginBtn: $("#loginBtn"),
     url: "http://u.xieqj.cn/UserVaildate.asmx/CrossLogin",
     init: function () {
         this.loginBtn.on("click", this.login);
     },
+    rtcInit: function () {
+        rtc.on("connected", function (socket) {
+            //创建本地视频流
+            rtc.createStream({
+                "video": true,
+                "audio": true
+            });
+        });
+        //创建本地视频流成功
+        rtc.on("stream_created", function (stream) {
+            document.getElementById('me').src = URL.createObjectURL(stream);
+            document.getElementById('me').play();
+        });
+        //创建本地视频流失败
+        rtc.on("stream_create_error", function () {
+            alert("create stream failed!");
+        });
+        //接收到其他用户的视频流
+        rtc.on('pc_add_stream', function (stream, socketId) {
+            var newVideo = document.createElement("video"),
+                id = "other-" + socketId;
+            newVideo.setAttribute("class", "other");
+            newVideo.setAttribute("autoplay", "autoplay");
+            newVideo.setAttribute("id", id);
+            videos.appendChild(newVideo);
+            rtc.attachStream(stream, id);
+        });
+        //删除其他用户
+        rtc.on('remove_peer', function (socketId) {
+            var video = document.getElementById('other-' + socketId);
+            if (video) {
+                video.parentNode.removeChild(video);
+            }
+        });
+       
+    },
     login: function () {
         var loginName = $("#loginName").val(), pwd = $("#pwd").val();
+        var iceName = $("#iceName").val(), icepwd = $("#icePwd").val();
         $.ajax({
-            url: userVaildateProvider.url,
+            url: pageObject.url,
             data: { loginName: loginName, pwd: pwd },
             type: 'post',
             cache: false,
@@ -19,10 +58,12 @@ var userVaildateProvider = {
             success: function (data) {
                 var r = data;
                 if (r.ActionResult) {
-                    userVaildateProvider.session = r.Data;
-                   // alert(userVaildateProvider.session);
+                    rtc = SkyRTC(iceName, icepwd);
+                    pageObject.rtcInit();
+                    pageObject.session = r.Data;
+                   // alert(pageObject.session);
                     //连接WebSocket服务器
-                    rtc.connect("ws:http://192.168.100.4:3000", "");
+                    rtc.connect("ws:http://v.xieqj.cn:3000", "");
                 }
                
             },
@@ -37,47 +78,4 @@ var userVaildateProvider = {
         });
     }
 };
-
-var videos = document.getElementById("videos");
-var rtc = SkyRTC();
-//成功创建WebSocket连接
-rtc.on("connected", function (socket) {
-    //创建本地视频流
-    rtc.createStream({
-        "video": true,
-        "audio": true
-    });
-});
-//创建本地视频流成功
-rtc.on("stream_created", function (stream) {
-    document.getElementById('me').src = URL.createObjectURL(stream);
-    document.getElementById('me').play();
-});
-//创建本地视频流失败
-rtc.on("stream_create_error", function () {
-    alert("create stream failed!");
-});
-//接收到其他用户的视频流
-rtc.on('pc_add_stream', function (stream, socketId) {
-    var newVideo = document.createElement("video"),
-        id = "other-" + socketId;
-    newVideo.setAttribute("class", "other");
-    newVideo.setAttribute("autoplay", "autoplay");
-    newVideo.setAttribute("id", id);
-    videos.appendChild(newVideo);
-    rtc.attachStream(stream, id);
-});
-//删除其他用户
-rtc.on('remove_peer', function (socketId) {
-    var video = document.getElementById('other-' + socketId);
-    if (video) {
-        video.parentNode.removeChild(video);
-    }
-});
-//接收到文字信息
-rtc.on('data_channel_message', function (channel, socketId, message) {
-    var p = document.createElement("p");
-    p.innerText = socketId + ": " + message;
-    msgs.appendChild(p);
-});
-userVaildateProvider.init();
+pageObject.init();
